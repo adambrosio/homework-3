fab2054e4bbce0e876f3dc59a6621f483279de80
Andrew D'Ambrosio <adambrosio20@gmail.com>
2020-04-09 19:17:24 -0400

    finalized JS

--- script.js
+++ script.js
@@ -3,32 +3,34 @@ var lowercaseEl = ["abcdefghijklmnopqrstuvwxyz"];
 var uppercaseEl = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 var numberEl = ["1234567890"];
 var symbolEl = ["!@#$%^&*()"];
+
+
 // Function to gather boolean information from user inputs
 function writePassword() {
   // Collecting user input
-  var passLen = parseInt(prompt("How many characters would you like your password to include? (Min = 8, Max = 128)"));
+  var length = parseInt(prompt("How many characters would you like your password to include? (Min = 8, Max = 128)"));
   // Checking if user input is entered as a number
-  if (isNaN(passLen) === true) {
+  if (isNaN(length) === true) {
     alert("Password length must be provided as a number");
     return;
   }
   // Checking to see if password length meets the criteria provided
-  if (passLen < 8 || passLen > 128) {
+  if (length < 8 || length > 128) {
     alert("Password length must be between 8 and 128 characters");
   }
   // Conditional statement to check if password length is at least 8 characters long. Prompts end if this evaluates false
-  if (passLen < 8) {
+  if (length < 8) {
     alert("Password length must be at least 8 characters");
     return;
   }
   // Conditional statement to check if password length is less than 128 characters long. Prompts end if this evaluates false
-  if (passLen > 128) {
+  if (length > 128) {
     alert("Password length must be less than 128 characters");
     return;
   }
   // Variable storing boolean from lowercase characters confirmation
   var lowercaseCon = confirm("Would you like to include lowercase characters?");
-  
+
   console.log(lowercaseCon);
   // Variable storing boolean from uppercase characters confirmation
   var uppercaseCon = confirm("Would you like to include uppercase characters?");
@@ -38,6 +40,7 @@ function writePassword() {
   var symbolCon = confirm("Would you like to include special characters?");
   // Empty array where characters will be populated
   var charArray = [];
+
   if (lowercaseCon === true) {
     charArray.push(lowercaseEl);
   }
@@ -61,29 +64,37 @@ function writePassword() {
       var position = (Math.floor(Math.random() * charactersLen));
       var result = charString.charAt(position);
       resultArray.push(result);
-      console.log(result);
-      // What in the actual fuck
-      console.log(position);
+      // console.log(result);
+      // console.log(position);
     }
-  }
-  
-  resultArray = [];
-  console.log(resultArray);
-  generatePassword(passLen);
-}
 
-document.getElementById("password").innerHTML = password;
-var generate = document.getElementById("generate");
-var password = document.getElementById("password");
+  }
+
+  resultArray = [];
+
+  console.log(resultArray);
+  generatePassword(length);
+  // Object for criteria to be stored
+  var passwordOptions = {
+    length: length,
+    lowercaseCon: lowercaseCon,
+    uppercaseCon: uppercaseCon,
+    numbersCon: numbersCon,
+    symbolCon: symbolCon,
+  }
+  // Appending password to password id textarea and converting to string
+  var string = resultArray.join("");
+  document.getElementById("password").append(string);
+  return passwordOptions;
+};
+
+// var generate = document.getElementById("generate");
+// var password = document.getElementById("password");
 generate.onclick = writePassword();
 
 function makePassword() {
   var password = generatePassword();
   var passwordText = ('textarea');
   passwordText.value = password;
-  // console.log(textarea);
-  // console.log(passwordText);
-  // console.log(password);
-  // console.log(generatePassword);
   console.log(charArray);
 }
